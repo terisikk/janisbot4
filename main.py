d863372f432a205a985247f4d16222d078718051
Teemu Risikko <teemu.risikko@gmail.com>
2019-12-06 14:38:43 +0200

    Remove filters for now

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ import janisbot4.handlers as handlers
 
 from aiogram import Bot, Dispatcher, executor
 from janisbot4.config import cfg
-from janisbot4.filters import ChatIdFilter
 
 
 TELEGRAM_API_TOKEN = cfg.get('telegram_api_token')
@@ -16,11 +15,8 @@ logging.basicConfig(level=int(os.environ.get('JANISBOT_LOGLEVEL', logging.INFO))
 bot = Bot(token=TELEGRAM_API_TOKEN)
 dp = Dispatcher(bot)
 
-chat_id_filter = ChatIdFilter(cfg.get('chat_ids'))
-dp.filters_factory.bind(ChatIdFilter, event_handlers=[dp.message_handlers])
-
-dp.register_message_handler(handlers.quote_command, chat_id_filter, commands=['quote'])
-dp.register_message_handler(handlers.quote_message, chat_id_filter, regexp='.*:$')
+dp.register_message_handler(handlers.quote_command, commands=['quote'])
+dp.register_message_handler(handlers.quote_message, regexp='.*:$')
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
