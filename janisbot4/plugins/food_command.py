ca96a1cbcc9fcead74d67bd60c9216e5837e2790
Teemu Risikko <teemu.risikko@gmail.com>
2022-01-03 10:13:07 +0200

    Add galaksi parsers for food

--- janisbot4/plugins/food_command.py
+++ janisbot4/plugins/food_command.py
@@ -1,6 +1,7 @@
 import requests
+import json
 
-from datetime import datetime
+from datetime import date
 from collections import namedtuple
 from bs4 import BeautifulSoup
 
@@ -25,32 +26,54 @@ async def get_food(restaurant_name):
     replytext = DEFAULT_REPLY
 
     if restaurant:
-        replytext = restaurant.parser(requests.get(restaurant.url))
+        try:
+            replytext = restaurant.parser(requests.get(restaurant.url()))
+        except Exception:
+            return DEFAULT_REPLY
 
     return replytext
 
 
 def automaatio_parser(response):
-    text = parse_response(response)
+    text = parse_response_text(response)
 
-    try:
-        soup = BeautifulSoup(text, features="html.parser")
-        container = soup.find("div", id="pgc-41-1-0")
+    soup = BeautifulSoup(text, features="html.parser")
+    container = soup.find("div", id="pgc-41-1-0")
 
-        weekday = get_finnish_day_name().capitalize()
-        food_today = container.find("h4", text=weekday).find_next_sibling()
+    weekday = get_finnish_day_name().capitalize()
+    food_today = container.find("h4", text=weekday).find_next_sibling()
 
-        return food_today.text
-    except Exception:
-        return DEFAULT_REPLY
+    return food_today.text
 
 
-def parse_response(response):
+
+def galaksi_parser(response):
+    jsondata = parse_response_json(response)
+
+    food_today = ""
+
+    for _, value in jsondata["courses"].items():
+        title = value.get("title_fi", "") 
+        dietcodes = value.get("dietcodes", "")
+
+        if dietcodes != "":
+            dietcodes = f" ({dietcodes})"
+
+        food_today += title + dietcodes + '\n'
+
+    return food_today
+
+
+def parse_response_text(response):
     return response.text if hasattr(response, "text") else response
 
 
+def parse_response_json(response):
+    return response.json if hasattr(response, "json") else json.loads(response)
+
+
 def get_finnish_day_name():
-    return WEEKDAYS[datetime.today().weekday()]
+    return WEEKDAYS[date.today().weekday()]
 
 
 WEEKDAYS = ["maanantai", "tiistai", "keskiviikko", "torstai", "perjantai", "lauantai", "sunnuntai"]
@@ -58,6 +81,9 @@ WEEKDAYS = ["maanantai", "tiistai", "keskiviikko", "torstai", "perjantai", "laua
 Restaurant = namedtuple("Restaurant", ["url", "parser"])
 RESTAURANTS = {
     "automaatio": Restaurant(
-        "https://www.aaltocatering.fi/automaatiotielounas/",
-        automaatio_parser)
+        lambda: "https://www.aaltocatering.fi/automaatiotielounas/",
+        automaatio_parser),
+    "galaksi": Restaurant(
+        lambda: "https://www.sodexo.fi/ruokalistat/output/daily_json/121/" + str(date.today()),
+        galaksi_parser)
 }
--- tests/plugins/test_food_command.py
+++ tests/plugins/test_food_command.py
@@ -9,20 +9,20 @@ from tests.message_stub import MessageStub
 
 @pytest.mark.asyncio
 async def test_food_list_is_returned_on_command(requests_mock):
-    with patch('janisbot4.plugins.food_command.datetime') as mock_date:
+    with patch('janisbot4.plugins.food_command.date') as mock_date:
         mock_date.today.return_value = date(2021, 12, 28)
         mock_date.side_effect = lambda *args, **kw: date(*args, **kw)
 
         url = "https://www.aaltocatering.fi/automaatiotielounas/"
 
-        requests_mock.get(url, text=TEST_DATA)
+        requests_mock.get(url, text=TEST_DATA_AUTOMAATIO)
 
         message = MessageStub()
         message.args = "automaatio"
 
         await food_command.index(message)
 
-        assert food_command.datetime.today() == date(2021, 12, 28)
+        assert food_command.date.today() == date(2021, 12, 28)
         assert message.replied != food_command.DEFAULT_REPLY
 
 
@@ -36,59 +36,69 @@ async def test_default_reply_on_nonexisting_restaurant(requests_mock, restaurant
     assert message.replied == food_command.DEFAULT_REPLY
 
 
-def test_default_reply_on_faulty_automaatio_content(requests_mock):
-    reply = food_command.automaatio_parser("empty text")
-    assert reply == food_command.DEFAULT_REPLY
+@pytest.mark.asyncio
+async def test_default_reply_on_faulty_content(requests_mock):
+    url = "https://www.aaltocatering.fi/automaatiotielounas/"
+
+    requests_mock.get(url, text="empty data")
+
+    replytext = await food_command.get_food("automaatio")
+
+    assert replytext == food_command.DEFAULT_REPLY
 
 
 def test_automaatio_restaurant_list_can_be_parsed():
-    with patch('janisbot4.plugins.food_command.datetime') as mock_date:
+    with patch('janisbot4.plugins.food_command.date') as mock_date:
         mock_date.today.return_value = date(2021, 12, 28)
         mock_date.side_effect = lambda *args, **kw: date(*args, **kw)
 
         expected = """\
-Sweet chili broileria ja nuudeleita M L,G
-Kokonaisena paistettua porsaansisäfilettä M,G (*Suomi)
-Bouillabaisse M,G
-Pizzabuffet
-Päivän kasvisruoka tilataan keittiöstä:
-Punajuuri-aurajuustovuoka L,G"""
+Test food 2
+Test food 3
+"""
 
-        food = food_command.automaatio_parser(TEST_DATA)
+        food = food_command.automaatio_parser(TEST_DATA_AUTOMAATIO)
         assert food == expected
 
 
-TEST_DATA = """
+def test_galaksi_restaurant_list_can_be_parsed():
+    expected = """\
+Test food 1 (M, G)
+Test food 2
+"""
+
+    food = food_command.galaksi_parser(TEST_DATA_GALAKSI)
+    assert food == expected
+
+
+TEST_DATA_AUTOMAATIO = """\
 <div id="pgc-41-1-0" class="panel-grid-cell">
 <div id="panel-41-1-0-0" class="so-panel widget widget_text panel-first-child panel-last-child" data-index="1">
 <div class="flash_inherit_color panel-widget-style panel-widget-style-for-41-1-0-0">
 <h3 class="widget-title">20.12-22.12.2021</h3>
 <div class="textwidget">
-<p>Lounas tarjolla klo 10.30-13.00<br>
-Tervetuloa!</p>
 <h4>Maanantai</h4>
-<p>Kermainen lihapata L,G<br>
-Paistettua kalaa L,G<br>
-Tomaatti-vuohenjuustokeittoa VL,G<br>
-Pizzabuffet<br>
-Päivän kasvisruoka tilataan keittiöstä:<br>
-Kasviskaalikääryleitä M,G</p>
+<p>Test food 1/p>
 <h4>Tiistai</h4>
-<p>Sweet chili broileria ja nuudeleita M L,G<br>
-Kokonaisena paistettua porsaansisäfilettä M,G (*Suomi)<br>
-Bouillabaisse M,G<br>
-Pizzabuffet<br>
-Päivän kasvisruoka tilataan keittiöstä:<br>
-Punajuuri-aurajuustovuoka L,G</p>
-<h4>Keskiviikko</h4>
-<p>Grillikylkeä M,G<br>
-Häränfilehöystöä L.G<br>
-Aurajuusto-kukkakaalikeittoa L,G (*Suomi)<br>
-Pizzabuffet<br>
-Päivän kasvisruoka tilataan keittiöstä:<br>
-Kasviscurrya M,G</p>
+<p>Test food 2<br>
+Test food 3<br>
+</p>
 </div>
 </div>
 </div>
 </div>
+"""
+
+TEST_DATA_GALAKSI = """
+{
+    "courses": {
+        "1": {
+            "title_fi": "Test food 1",
+            "dietcodes": "M, G"
+        },
+        "2": {
+            "title_fi": "Test food 2"
+        }
+    }
+}
 """
