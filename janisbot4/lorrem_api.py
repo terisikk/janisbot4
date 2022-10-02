bd4d6584982126722f9b2a75efa5f9030a426c0e
Teemu Risikko <teemu.risikko@gmail.com>
2022-10-02 18:15:18 +0300

    Add authentication to lorrem api

--- janisbot4/lorrem_api.py
+++ janisbot4/lorrem_api.py
@@ -4,12 +4,15 @@ from janisbot4.config import cfg
 
 
 LORREM_API_URL = cfg.get('lorrem_api_url')
+LORREM_API_TOKEN = cfg.get('lorrem_api_token')
 
 EMPTY_RESPONSE = '???'
 
+HEADERS = {'Authorization': LORREM_API_TOKEN}
+
 
 def request(request_str):
-    response = requests.get(f'{LORREM_API_URL}/{request_str}')
+    response = requests.get(f'{LORREM_API_URL}/{request_str}', headers=HEADERS)
     return response.json()
 
 
